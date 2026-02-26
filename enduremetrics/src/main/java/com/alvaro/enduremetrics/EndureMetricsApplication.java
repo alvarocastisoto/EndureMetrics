1c1e7430108e028ea274c03aea477ef75961b3e3
alvarocastisoto <alvarocastisoto@gmail.com>
2026-02-26 15:41:29 +0100

    Añadido los datos al perfil del usuario

---
+++ enduremetrics/src/main/java/com/alvaro/enduremetrics/EndureMetricsApplication.java
@@ -0,0 +1,45 @@
+package com.alvaro.enduremetrics;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import atlantafx.base.theme.PrimerLight;
+
+@SpringBootApplication
+public class EndureMetricsApplication extends Application {
+
+	private ConfigurableApplicationContext springContext;
+
+	public static void main(String[] args) {
+		Application.launch(EndureMetricsApplication.class, args);
+	}
+
+	@Override
+	public void init() {
+		springContext = SpringApplication.run(EndureMetricsApplication.class);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/login-view.fxml"));
+		fxmlLoader.setControllerFactory(springContext::getBean);
+		Parent root = fxmlLoader.load();
+		Scene scene = new Scene(root);
+		stage.setTitle("EndureMetrics");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	@Override
+	public void stop() {
+		springContext.close();
+		Platform.exit();
+	}
+}
